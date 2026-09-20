287d23fbf1b1a5bb227c9764e14dd8b87b8a53e8
fammistare05-wq <fammistare05@gmail.com>
2026-09-20 13:34:02 +0200

    Add files via upload

--- palestra/sw.js
+++ palestra/sw.js
@@ -1,4 +1,4 @@
-const CACHE="palestra-v4";
+const CACHE="palestra-v6";
 const CORE=["./","index.html","manifest.webmanifest","icon-192.png","icon-512.png","icon-maskable-512.png","apple-touch-icon.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
